bcdf9f9ae795eea48ecb0ce0fc5d3eb3a0b3a895
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-06-18 20:03:36 -0700

    completed time display assignment

--- apps/timedisplay/views.py
+++ apps/timedisplay/views.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.shortcuts import render #, HttpResponse
 
 # Create your views here.
 def index(request):
     print '$'*50
-    return render(request, 'timedisplay/index.html')
+    now = datetime.today()
+    context = {
+        "date": now.strftime('%b %d, %Y'),
+        "time": now.strftime('%I:%M %p')
+    }
+    return render(request, 'timedisplay/index.html', context)
